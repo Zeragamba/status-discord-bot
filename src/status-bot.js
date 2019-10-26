cb57d0546dd35d2aba11c9875a26e0ae0b414ae5
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-10-26 12:43:56 -0400

    Configure core bot stuff

--- src/status-bot.js
+++ src/status-bot.js
@@ -1,7 +1,40 @@
 const ChaosCore = require('chaos-core');
+const Path = require('path');
+
+const packageJson = require('../package');
+
+const plugins = [
+
+];
+
+const defaultConfig = {
+  ownerUserId: null,
+  loginToken: null,
+
+  logger: {
+    level: 'info',
+  },
+
+  dataSource: {
+    type: 'disk',
+    dataDir: Path.join(__dirname, '../data'),
+  },
+};
 
 class StatusBot extends ChaosCore {
+  constructor(config) {
+    super({
+      ...defaultConfig,
+      ...config,
+      plugins,
+    });
 
+    this.on('chaos.listen', () => this.discord.user.setPresence({
+      game: {
+        name: `v${packageJson.version}`,
+      },
+    }));
+  }
 }
 
 module.exports = StatusBot;
